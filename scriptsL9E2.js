bc9b492c016bc27171fa894fe39425db0eb1ea68
Jasman <jd15nc@brocku.ca>
2018-11-09 22:26:51 -0500

    Lab 9

---
+++ scriptsL9E2.js
@@ -0,0 +1,16 @@
+//Sets the initial colours of the three elements on page load
+document.getElementById("blue").style.color="blue";
+document.getElementById("green").style.color="green";
+document.getElementById("red").style.color="red";
+
+//A function to change colours of elements when called
+function changeColour(){
+
+	var first = "#1a1aff";
+	var second = "#33cc33";
+	var third = "#ff0000";
+
+	document.getElementById("blue").style.color=first;
+	document.getElementById("green").style.color=second;
+	document.getElementById("red").style.color=third;
+}
